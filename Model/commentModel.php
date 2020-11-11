4413a6a93bbabb20cdf2174a1d4a47c9d9ce63eb
juanpedroarmella <juanpedroarmella@gmail.com>
2020-11-11 12:00:29 -0300

    Agregado delete coment en model

--- Model/commentModel.php
+++ Model/commentModel.php
@@ -8,5 +8,10 @@ class commentModel{
         $this->db = new PDO('mysql:host=localhost;'.'dbname=store;charset=utf8', 'root', '');
     }
 
+    function deleteComent($id_coment){
+        $sentencia = $this->db->prepare("DELETE FROM product WHERE id_coment = ?");
+        $sentencia-> execute(array($id_category));
+    }
+
 
     
